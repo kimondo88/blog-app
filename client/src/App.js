b190d23b4a0520aa12270263a0385919a5986c21
kimondo88 <kimondo1988@o2.pl>
2022-03-31 09:17:09 +0200

    add content, navbar features also change in app render

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import Nav from './feature/navbar/navbar'
+import Content from './feature/content/content'
 function App() {
   return (
     <div className="App">
-      
+      <Nav/>
+      <Content/>
     </div>
   );
 }
---
+++ client/src/feature/navbar/navbar.js
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+export default function Navbar(){
+    return(
+        <nav className="nav">
+            <div className="left">
+                <div className='start'>Click Me</div>
+                <div className='start'>Click Me</div>
+            </div>
+            <div className="log">Profile</div>
+        </nav>
+    )
+}
